ed074ded391712360ce5f50fbeabace06f586855
Orlando Montoya Benitez <orlando.montoyab@perficient.com>
2022-09-08 12:13:12 -0500

    Add ChartBars to the app

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -21,7 +21,7 @@ const ExpenseForm = (props) => {
 
     const newExpenseData = {
       title: enteredTitle,
-      amount: enteredAmount,
+      amount: +enteredAmount,
       date: new Date(enteredDate),
     };
 
